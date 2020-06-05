959deda60af3961c61d6337572f5ee3df2f31021
Martinlud <hectormartinluduena@gmail.com>
2020-06-05 19:20:25 -0300

    handleChange

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,11 @@ class TodoItem extends Component {
     render() { 
         return ( 
             <div className='todo-item'>
-                <input type="checkbox" checked={this.props.item.completed}/>
+                <input 
+                    type="checkbox" 
+                    checked={this.props.item.completed}
+                    onChange={()=>this.props.handleChange(this.props.item.id)}
+                    />
                 <p>{this.props.item.text}</p>
             </div>
          );
